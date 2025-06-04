e01b2bcf915f66c2035ad6cb674a64fe0037d1c5
Rozdyma <rozdymka420@gmail.com>
2025-06-04 11:13:38 +0200

    Update luko.js

--- luko.js
+++ luko.js
@@ -1,2 +1,2 @@
-document.write("ostatnia modyfikacja strony".fontcolor("#FFFF00").bold().fontsize(7)+"<br>"); 
+document.write("ostatnia modyfikacja strony".fontcolor("#FFFF00").style.textDecoration = "line-through".fontsize(7)+"<br>"); 
 document.write(document.lastModified);
